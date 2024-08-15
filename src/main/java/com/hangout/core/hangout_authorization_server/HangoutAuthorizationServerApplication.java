8d06a81e48620de7726c6651b4cb2f805cea070f
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-08-15 20:19:41 +0530

    Initial Commit

---
+++ src/main/java/com/hangout/core/hangout_authorization_server/HangoutAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.hangout.core.hangout_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoutAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangout/core/hangout_authorization_server/HangoutAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout.core.hangout_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
